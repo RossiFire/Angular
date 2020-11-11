2e18c086be8b90d2e6e8da5bf4a9e248dcb90961
RossiFire <fratsavio.dr@gmail.com>
2020-11-11 15:25:14 +0100

    update

--- src/app/TableInfo.ts
+++ src/app/TableInfo.ts
@@ -2,3 +2,10 @@ export interface TbData{
     key : number
     label : string
 } 
+
+/* export const tbInfo : TbData[] =[
+    {key : 1 , label : "gino"},
+    {key : 2 , label : "pino"},
+    {key : 3 , label : "lino"},
+    {key : 4 , label : "fino"},
+] */
